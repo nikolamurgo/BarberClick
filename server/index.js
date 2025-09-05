178f5738efee5de23b10cdcb01585608a91b5a95
nikolamurgo <murgovskinikola@gmail.com>
2025-09-05 23:26:10 +0200

    update: set port server

--- server/index.js
+++ server/index.js
@@ -7,7 +7,11 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3001
+
+app.get("/", (req,res)=>{
+    res.send("api runs")
+})
 
 app.listen(PORT, () => 
     console.log(`Server running on port ${PORT}`)
