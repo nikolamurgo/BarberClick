720a0a75671b5c98cbebc566620470d430411d29
nikolamurgo <murgovskinikola@gmail.com>
2025-09-08 03:50:09 +0200

    test db removed

--- server/config/database.js
+++ server/config/database.js
@@ -8,15 +8,4 @@ const supabaseKey = process.env.SUPABASE_KEY
 // create supabase client
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-const testConnection = async () =>{
-    try{
-        await supabase.from('test').select('*')
-        console.log("Connected to db successfully")
-    }catch(err){
-        console.error("dbcon failed", err.message)
-    }
-}
-
-testConnection()
-
 module.exports = supabase
